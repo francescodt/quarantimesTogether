1275d8c377173e7f2257c56178ddd28bc779f078
scottbisese <bisese.scott@gmail.com>
2020-04-23 14:51:14 -0500

    added users sql

---
+++ users.sql
@@ -0,0 +1,15 @@
+CREATE TABLE IF NOT EXISTS users (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(100) NOT NULL UNIQUE
+);
+
+ALTER TABLE tasks
+    ADD COLUMN user_id INT;
+
+INSERT INTO users (username)
+VALUES ('scott');
+
+UPDATE tasks SET user_id = 1;
+
+ALTER TABLE tasks ADD CONSTRAINT fk_user_id
+FOREIGN KEY (user_id) REFERENCES users(id);
